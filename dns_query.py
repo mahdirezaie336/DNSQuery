03c7ecab3c2e8ceed3d7e554e40c78a5d8fe6099
mahdi <mahdi.rezaie.336@gmail.com>
2021-04-02 14:36:54 +0430

    Add get type to DNSQuery

--- dns_query.py
+++ dns_query.py
@@ -5,21 +5,38 @@ import main
 
 class DNSQuery:
 
-    def __init__(self, address, query_id = b'\xee\xee'):
-        self.address = address
+    def __init__(self, address, type='A', query_id='eeee'):
         self.id = query_id
-        self.fields = ['0',         # QR
-                       '0000',      # OPCODE
-                       '0',         # AA
-                       '0',         # TC
-                       '1',         # RD
-                       '0',         # RA
-                       '000',       # Z
-                       '0000'       # RCODE
-                       ]
+        self.fields = ['0',  # QR
+                       '0000',  # OPCODE
+                       '0',  # AA
+                       '0',  # TC
+                       '1',  # RD
+                       '0',  # RA
+                       '000',  # Z
+                       '0000'  # RCODE
+        ]
+        self.counts = [
+            1,  # QDCOUNT
+            0,  # ANCOUNT
+            0,  # NSCOUNT
+            0  # ARCOUNT
+        ]
+        self.question_field = [
+            address,                                    # QNAME
+            DNSQuery.get_record_type_value(type),       # QTYPE
+            1                                           # QCLASS
+        ]
 
     def get_message(self):
+        pass
 
+    @staticmethod
+    def get_record_type_value(record_type):
+        types = ["ERROR", "A", "NS", "MD", "MF", "CNAME", "SOA", "MB", "MG", "MR", "NULL", "WKS", "PTS", "HINFO",
+                 "MINFO", "MX", "TXT"]
+
+        return types.index(record_type) if isinstance(record_type, str) else types[record_type]
 
 
 dns_server = '1.1.1.1'
