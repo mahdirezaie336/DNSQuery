cad617c868da57a44701f7b1ce17f420edef1872
mahdi <mahdi.rezaie.336@gmail.com>
2021-04-02 17:28:41 +0430

    Change DNSQuery Fields to header and question

--- dns_query.py
+++ dns_query.py
@@ -5,31 +5,33 @@ import main
 
 class DNSQuery:
 
-    def __init__(self, address, type='A', query_id='eeee'):
-        self.id = query_id
-        self.fields = ['0',  # QR
-                       '0000',  # OPCODE
-                       '0',  # AA
-                       '0',  # TC
-                       '1',  # RD
-                       '0',  # RA
-                       '000',  # Z
-                       '0000'  # RCODE
-        ]
-        self.counts = [
-            1,  # QDCOUNT
-            0,  # ANCOUNT
-            0,  # NSCOUNT
-            0  # ARCOUNT
+    def __init__(self, address, type='A', q_class=1, query_id='eeee'):
+        self.header_fields = [
+            '{:04x}'.format(int(id, 16)),           # ID
+            '{:04x}'.format(int(''.join((
+                '0',                                # QR
+                '0000',                             # OPCODE
+                '0',                                # AA
+                '0',                                # TC
+                '1',                                # RD
+                '0',                                # RA
+                '000',                              # Z
+                '0000'                              # RCODE
+            )), 2)),
+            '{:04x}'.format(1),                    # QDCOUNT
+            '{:04x}'.format(0),                    # ANCOUNT
+            '{:04x}'.format(0),                    # NSCOUNT
+            '{:04x}'.format(0)                     # ARCOUNT
         ]
         self.question_field = [
             address,                                    # QNAME
             DNSQuery.get_record_type_value(type),       # QTYPE
-            1                                           # QCLASS
+            q_class                                     # QCLASS
         ]
 
-    def get_message(self):
-        pass
+    def generate_message(self):
+        message = ''.join(self.header_fields)
+
 
     @staticmethod
     def get_record_type_value(record_type):
